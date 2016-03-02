852dbe36ed04f9011ad90416c16be3978f7d6f6b
Willy Ko <willyko@y7mail.com>
2016-03-02 02:05:18 -0800

    rails_apps_composer: add README files

--- app/models/visitor.rb
+++ app/models/visitor.rb
@@ -1,2 +1,10 @@
 class Visitor < ActiveRecord::Base
+  validates_presence_of :email
+  validates_format_of :email, :with => /\A[-a-z0-9_+\.]+\@([-a-z0-9]+\.)+[a-z0-9]{2,4}\z/i
+  after_create :subscribe
+
+  def subscribe
+    MailingListSignupJob.perform_later(self)
+  end
+
 end
